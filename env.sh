3f70017a5405e9f6d8b915b1f800ccf77fbe294c
Assaf Lavie <me@assaflavie.com>
2018-02-26 08:22:50 +0200

    probably best to use a real api key, rather than the realm name

--- env.sh
+++ env.sh
@@ -1,4 +1,3 @@
-export BINARIS_API_KEY=$realm
 export BINARIS_INVOKE_ENDPOINT=run-$realm.binaris.io
 export BINARIS_DEPLOY_ENDPOINT=api-$realm.binaris.io
 export BINARIS_LOG_ENDPOINT=logs-$realm.binaris.io
