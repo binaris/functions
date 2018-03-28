9f35b20efc95e776ff6a42eb16ffb6186b215fc8
Assaf Lavie <me@assaflavie.com>
2018-03-28 10:54:17 +0300

    A word about when to use env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
+# No need to source this file by default. Only if you want to work against non-production (or customer-specifie) Binaris environments (realms).
+
 export BINARIS_INVOKE_ENDPOINT=run-$realm.binaris.io
 export BINARIS_DEPLOY_ENDPOINT=api-$realm.binaris.io
 export BINARIS_LOG_ENDPOINT=logs-$realm.binaris.io
